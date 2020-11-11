746498db84da7f1ba89b6ad255c172e0a0740e26
Dyrlien <46710693+Dyrlien@users.noreply.github.com>
2020-11-11 17:38:58 +0100

    design oppdatert, kontakt oss påbegynt

--- WebAppsOppgave3/WebAppsOppgave3/wwwroot/js/FAQ.js
+++ WebAppsOppgave3/WebAppsOppgave3/wwwroot/js/FAQ.js
@@ -1,21 +1,26 @@
 ﻿var allCategories = [];
 var allQuestions = [];
 
+
 $(document).ready(function () {
     getAllCategories();
+    getAllQuestions(1);
+    getSelectedQuestion(1);
 });
 
 //Aquires all categories from the database
 function getAllCategories() {    
     $.get("FAQ/GetEveryCategories", function (output) {        
-        console.log(output);
+        
+        allCategories = output;
+        console.log(allCategories.toString());
         printAllCategories(output);
     });
 }
 
 //Prints all Categories to the interface
 function printAllCategories(categories) {    
-    let stringbuilderCategories = '<ul class="list-group">';
+    let stringbuilderCategories = '<h4>Kategorier</h4><ul class="list-group">';
     categories.forEach(element => stringbuilderCategories += '<li class="list-group-item" onclick="getAllQuestions('
         + element.id + ')"><h5>' + element.categoryName + '</h5></li>');
     stringbuilderCategories += '</ul>';
@@ -31,7 +36,7 @@ function getAllQuestions(categoryId) {
 }
 
 function getSelectedCategory(questions, categoryId) {
-    let stringbuilderQuestions = '<ul class="list-group">';
+    let stringbuilderQuestions = '<h4>Spørsmål</h4><ul class="list-group">';
     for (let q of questions) {
         if (q.category.id === categoryId) {
             stringbuilderQuestions += '<li class="list-group-item" onclick="getSelectedQuestion('
@@ -41,4 +46,26 @@ function getSelectedCategory(questions, categoryId) {
     console.log(stringbuilderQuestions);
     stringbuilderQuestions += '</ul>';
     $("#FAQQuestions").html(stringbuilderQuestions);
+}
+
+function getSelectedQuestion(questionId) {
+    let answer;
+    $.get("FAQ/GetEveryQuestion", function (output) {
+        answerElement = output.find(element => element.id == questionId);
+        printSelectedAnswer(answerElement.answer);
+    });
+}
+
+function printSelectedAnswer(answer) {
+    let stringbuilderAnswer = '<p>' + answer + '</p>';
+    $("#FAQAnswer").html(stringbuilderAnswer);
+    
+}
+
+//OVERLAY
+function openQuestionOverlay() {
+    $("#questionOverlay").css('width', '100%');
+}
+function closeQuestionOverlay() {
+    $("#questionOverlay").css('width', '0%');
 }
